c18bad7f9b20a3d13d44769392cfc42a04e3b283
Scott Gibbs <scott@gibbz.biz>
2020-10-22 22:52:00 -0600

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { h, render } from 'preact';
+import './index.css';
+
+render(<App />, document.getElementById('root') as HTMLElement)
+
+function App() {
+    return <div class="h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <h1 class="text-2xl">Welcome</h1>
+        <p>You've done it!</p>
+    </div >
+}
