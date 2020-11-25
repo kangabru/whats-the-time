ca65f8286c379513ac3994d1978b644639f64fc1
Scott Gibbs <scott@gibbz.biz>
2020-11-24 18:19:26 -0600

    Upgraded to Parcel V2 and Tailwind CSS V2.

--- src/index.tsx
+++ src/index.tsx
@@ -7,5 +7,5 @@ function App() {
     return <div class="h-screen bg-gray-100 flex flex-col items-center justify-center">
         <h1 class="text-2xl">Welcome</h1>
         <p>You've done it!</p>
-    </div >
+    </div>
 }
