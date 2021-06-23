b544ee8a9f2bb07fa6af3f6ac2458e1b66ed7141
Scott Gibbs <scott@gibbz.biz>
2021-06-23 10:55:27 -0600

    Fixed issued where the timezone select input was being hidden by the modals 'overflow hidden' property.

--- src/components/menus/timezone-edit.tsx
+++ src/components/menus/timezone-edit.tsx
@@ -24,7 +24,7 @@ export default function CreateTimezone({ isOpen, close }: { isOpen: boolean, clo
     return <Transition appear show={isOpen} as={Fragment}>
         <Dialog
             as="div"
-            className="fixed inset-0 z-10 overflow-y-auto"
+            className="fixed inset-0 z-10"
             onClose={close}
         >
             <div className="col justify-center min-h-screen px-4 text-center">
@@ -50,7 +50,7 @@ export default function CreateTimezone({ isOpen, close }: { isOpen: boolean, clo
                     leaveFrom="opacity-100 scale-100"
                     leaveTo="opacity-0 scale-95"
                 >
-                    <div className="inline-block w-full max-w-md p-6 my-8 space-y-4 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                    <div className="inline-block w-full max-w-md p-6 my-8 space-y-4 text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                         <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                             Create timezone
                         </Dialog.Title>
