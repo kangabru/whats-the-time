ef407738ba3d94319d5bb56eafb284bc024a302f
Scott Gibbs <scott@gibbz.biz>
2021-06-23 11:23:10 -0600

    Cleaned up timezone code

--- src/components/menus/timezone.tsx
+++ src/components/menus/timezone.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon, ChevronUpIcon, MenuIcon, PencilIcon, TrashIcon } from '@heroicons/react/solid'
-import { Fragment, h, JSX } from 'preact'
+import { Fragment, h, JSX, Ref } from 'preact'
 import { useRelativeBounds } from '../../utils/relative-context'
 import useAppState from '../../utils/store'
 import { Location } from '../../utils/types'
@@ -8,42 +8,35 @@ import { join } from '../../utils/utils'
 
 export default function TimezoneMenu({ location }: { location: Location }) {
     const { moveLocation, removeLocation } = useAppState()
-
-    const [ref, { y, height }] = useRelativeBounds()
-    const dy = y + height / 2
-
+    const [ref, top] = useMenuPosition()
     return <Menu as="div" className="inline-block text-left">
-        {/* @ts-ignore */}
-        {({ open }) => <>
-            <Menu.Button ref={ref} title="Edit" className="flex-shrink-0 rounded focus:outline-none focus:ring-2 focus:ring-gray focus:ring-opacity-75 opacity-50 hover:opacity-100">
-                <MenuIcon class="h-6 w-6" />
-            </Menu.Button>
-            <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="opacity-0 scale-90 -translate-x-2"
-                enterTo="opacity-100 scale-100 translate-x-0"
-                leave="transition ease-in duration-75"
-                leaveFrom="opacity-100 scale-100 translate-x-0"
-                leaveTo="opacity-0 scale-90 -translate-x-2"
-            >
-                <Menu.Items
-                    style={{ '--tw-translate-y': `calc(${dy}px - 50%)` }}
-                    className="absolute z-20 top-0 left-14 transform w-32 bg-white origin-left divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="px-1 py-1 ">
-                        <MenuButton text="Edit" onClick={console.log} icon={PencilIcon} />
-                    </div>
-                    <div className="px-1 py-1">
-                        <MenuButton text="Move up" onClick={() => moveLocation(location, true)} icon={ChevronUpIcon} />
-                        <MenuButton text="Move down" onClick={() => moveLocation(location, false)} icon={ChevronDownIcon} />
-                    </div>
-                    <div className="px-1 py-1">
-                        <MenuButton text="Delete" onClick={() => removeLocation(location)} icon={TrashIcon} class="text-red-600" classActive="bg-red-100 underline" />
-                    </div>
-                </Menu.Items>
-            </Transition>
-        </>}
+        <Menu.Button ref={ref} title="Edit" className="flex-shrink-0 rounded focus:outline-none focus:ring-2 focus:ring-gray focus:ring-opacity-75 opacity-50 hover:opacity-100">
+            <MenuIcon class="h-6 w-6" />
+        </Menu.Button>
+        <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="opacity-0 scale-90 -translate-x-2"
+            enterTo="opacity-100 scale-100 translate-x-0"
+            leave="transition ease-in duration-75"
+            leaveFrom="opacity-100 scale-100 translate-x-0"
+            leaveTo="opacity-0 scale-90 -translate-x-2"
+        >
+            <Menu.Items
+                style={{ '--tw-translate-y': `calc(${top}px - 50%)` }}
+                className="absolute z-20 top-0 left-14 transform w-32 bg-white origin-left divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                    <MenuButton text="Edit" onClick={console.log} icon={PencilIcon} />
+                </div>
+                <div className="px-1 py-1">
+                    <MenuButton text="Move up" onClick={() => moveLocation(location, true)} icon={ChevronUpIcon} />
+                    <MenuButton text="Move down" onClick={() => moveLocation(location, false)} icon={ChevronDownIcon} />
+                </div>
+                <div className="px-1 py-1">
+                    <MenuButton text="Delete" onClick={() => removeLocation(location)} icon={TrashIcon} class="text-red-600" classActive="bg-red-100 underline" />
+                </div>
+            </Menu.Items>
+        </Transition>
     </Menu>
 }
 
@@ -66,4 +59,10 @@ function MenuButton({ text, icon, onClick, class: cls, classActive }: MenuButton
             </button>
         )}
     </Menu.Item>
+}
+
+function useMenuPosition(): [Ref<HTMLElement>, number] {
+    const [ref, { y, height }] = useRelativeBounds()
+    const top = y + height / 2
+    return [ref, top]
 }
