924556f6a42252ad34d6b6b78471331c3ac74492
Scott Gibbs <scott@gibbz.biz>
2021-06-23 21:10:24 -0600

    Fixed the time selector to find the init value in the picklist

--- src/components/selectors/time.tsx
+++ src/components/selectors/time.tsx
@@ -5,14 +5,17 @@ import times from '../../data/times';
 import { Time } from '../../utils/types';
 import Selector from './selector';
 
+const DEFAULT: Time = { hour: 13 }
+
 export type TimeOption = {
     name: string,
     time: Time,
 }
 
-export default function useTimeSelector(defaultTime: Time = { hour: 13 }): [JSX.Element, TimeOption] {
+export default function useTimeSelector(defaultTime: Time = DEFAULT): [JSX.Element, TimeOption] {
     const options = useMemo(() => times.map(toTimeOption), [])
-    const [time, setTime] = useState<TimeOption>(toTimeOption(defaultTime))
+    const defaultOption = useFindTimeOption(options, defaultTime)
+    const [time, setTime] = useState<TimeOption>(defaultOption)
     return [
         <Selector<TimeOption>
             options={options} value={time} onChange={setTime}
@@ -34,4 +37,11 @@ export function useNow(): Time {
         const hour = now.hour + (now.minute <= 30 ? 0 : 1)
         return { hour: hour % 24 }
     }, [])
+}
+
+export function useFindTimeOption(options: TimeOption[], time: Time = DEFAULT) {
+    return useMemo(() => {
+        const option = options.find(o => o.time.hour === time.hour)
+        return option ?? toTimeOption(time)
+    }, [time])
 }
