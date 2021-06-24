f193e10a1a1d2ade62a2b0035ce2401f772f09c3
Scott Gibbs <scott@gibbz.biz>
2021-06-23 21:10:45 -0600

    Improved the 'convert time' component layout

--- src/components/convert.tsx
+++ src/components/convert.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon';
 import { h } from 'preact';
 import useAppState from '../utils/store';
-import { prettyTime } from '../utils/utils';
+import { join, prettyTime } from '../utils/utils';
 import useTimeSelector, { useNow as useLocalNow } from './selectors/time';
 import { useTimezoneSelector } from './selectors/timezone';
 
@@ -16,27 +16,35 @@ export default function TimeConverter() {
     const _timeWhen = DateTime.fromObject({ zone: timezoneWhen.timezone, hour: timeWhen.time.hour, minute: 0, second: 0 })
     const _timeIn = _timeWhen.setZone(timezoneIn.timezone)
 
+    const classText = "w-full px-2 py-1 text-left"
+    const classTextTiny = "absolute bottom-full -left-4 text-sm"
+    const classSelect = "px-2 py-1 text-base relative"
+
     // e.g. Time in New York when 10 pm in Londong
-    return <table>
-        <tbody>
-            <tr>
-                <td class="px-2 py-1 text-center">Time in</td>
-                <td class="px-2 py-1">{selectorTimezoneIn}</td>
-            </tr>
-            <tr>
-                <td class="px-2 py-1 text-center">when</td>
-                <td class="px-2 py-1">{selectorTimeWhen}</td>
-            </tr>
-            <tr>
-                <td class="px-2 py-1 text-center">in</td>
-                <td class="px-2 py-1">{selectorTimezoneWhen}</td>
-            </tr>
-            <tr>
-                <td colSpan={2} class="px-2 py-1 text-center">
-                    <hr class="w-full my-2 border-t-2 border-gray-200" />
-                    <span>{prettyTime(_timeIn, _timeWhen)}</span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    return <div class="col">
+
+        <h2 class={join(classText, 'text-center text-4xl font-extrabold mb-10')}>What's the time?</h2>
+
+        <div class={classSelect}>
+            <div class={classTextTiny}>in</div>
+            {selectorTimezoneIn}
+        </div>
+
+        <div class="space-y-4 mt-14">
+            <div class={classSelect}>
+                <div class={classTextTiny}>when it's</div>
+                {selectorTimeWhen}
+            </div>
+
+            <div class={classSelect}>
+                <div class={classTextTiny}>in</div>
+                {selectorTimezoneWhen}
+            </div>
+        </div>
+
+        <div colSpan={2} class={join(classText, 'w-full mt-6 text-4xl text-center font-extrabold')}>
+            <span>{prettyTime(_timeIn, _timeWhen)}</span>
+        </div>
+
+    </div>
 }
