8625246b301b05f3a54aa6fda04f3bc86945f1a9
Scott Gibbs <scott@gibbz.biz>
2021-06-26 21:31:03 -0600

    Added ability to inject the site url via environment variables

---
+++ posthtml.config.js
@@ -0,0 +1,13 @@
+/** We use Parcel and PostHTML with the 'posthtml-expressions' plugin to populate environment variables in HTML files.
+ * @see https://github.com/parcel-bundler/parcel/issues/1209#issuecomment-432424397
+ */
+
+module.exports = {
+    plugins: {
+        "posthtml-expressions": {
+            locals: {
+                URL_SITE: process.env.URL_SITE,
+            }
+        }
+    }
+}
